ec4d05776e0c03f8de2b38da1e2cc32a2edaeeb7
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-20 12:51:42 -0700

    created tables for data

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,38 @@
 exports.up = async (knex) => {
   await knex.schema
+    .createTable('roles', (roles) => {
+      roles.increments('role_id')
+      roles.string('role_name').notNullable()
+    })
     .createTable('users', (users) => {
       users.increments('user_id')
       users.string('username', 200).notNullable()
       users.string('password', 200).notNullable()
+      users.string('email', 200).notNullable().unique()
       users.timestamps(false, true)
+      users.integer('role')
+           .unsigned()
+           .references('roles.role_id')
+           .onDelete('RESTRICT')
+           .onUpdate('RESTRICT')
+           .defaultTo(2)
+    })
+    .createTable('instructor', (instructor) => {
+      instructor.increments('class_id')
+      instructor.string('class_name', 200).notNullable()
+      instructor.string('class_type', 200).notNullable()
+      instructor.datetime('class_date', {precision: 6 }).notNullable()
+      instructor.string('duration', 200).notNullable()
+      instructor.string('intensity_level', 200).notNullable()
+      instructor.string('location', 200).notNullable()
+      instructor.integer('attendees', 100).notNullable()
+      instructor.integer('max_size', 100).notNullable()
     })
 }
+  
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('instructor')
   await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists('roles')
 }
